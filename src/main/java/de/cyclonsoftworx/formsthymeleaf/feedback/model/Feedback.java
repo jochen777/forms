0caa742f695598943de26bed4221cdd7460e4926
jochen <jpier@me.com>
2020-02-29 00:10:39 +0100

    Handle Errors

--- src/main/java/de/cyclonsoftworx/formsthymeleaf/feedback/model/Feedback.java
+++ src/main/java/de/cyclonsoftworx/formsthymeleaf/feedback/model/Feedback.java
@@ -2,14 +2,23 @@ package de.cyclonsoftworx.formsthymeleaf.feedback.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
 public class Feedback {
     String firstname;
+
+    @NotNull
+    @Size(min=2, max=30)
     String lastname;
+
     LocalDate birthday;
+
     String email;
+
     String text;
+
     boolean optin;
 }
--- src/main/java/de/cyclonsoftworx/formsthymeleaf/feedback/web/FeedbackController.java
+++ src/main/java/de/cyclonsoftworx/formsthymeleaf/feedback/web/FeedbackController.java
@@ -2,8 +2,12 @@ package de.cyclonsoftworx.formsthymeleaf.feedback.web;
 
 import de.cyclonsoftworx.formsthymeleaf.feedback.model.Feedback;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class FeedbackController {
@@ -11,5 +15,15 @@ public class FeedbackController {
     @GetMapping("/feedback")
     public String form(Feedback feedback) {
         return "feedback/form";
+    }
+
+    @PostMapping("/feedback")
+    public String checkPersonInfo(@Valid Feedback form, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "feedback/form";
+        }
+
+        return "redirect:/results";
     }
 }
